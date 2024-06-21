c83b41d32eca14f0c15f0dceee5207f605344091
akramznini <akramznini@gmail.com>
2024-06-21 01:10:59 -0400

    CarPreviewContainer and CarInfoContainer completed.

--- src/kimo/lib/main.dart
+++ src/kimo/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -93,9 +94,17 @@ class _MyHomePageState extends State<MyHomePage> {
           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Expanded(child: Placeholder(),),
+            Row(
+              children: [
+                CarPreviewContainer(),
+                CarPreviewContainer(),
+                CarPreviewContainer(),
+              ],
+            ),
+
             Padding(
               padding: const EdgeInsets.only(top: 10, bottom: 10),
               child: Row(
@@ -104,14 +113,82 @@ class _MyHomePageState extends State<MyHomePage> {
                   
                   GestureDetector(onTap: () {_onChangedTab(0);}, child: NavigationDestination(selected: _selectedTab == 0, svgPathSelected: "assets/icons/navbar-home-selected.svg" , svgPath: "assets/icons/navbar-home.svg", label: "HOME",)),
                   GestureDetector(onTap: () {_onChangedTab(1);}, child: NavigationDestination(selected: _selectedTab == 1, svgPathSelected: "assets/icons/navbar-wishlist-selected.svg" , svgPath: "assets/icons/navbar-wishlist.svg", label: "WISHLIST",)),
-                  GestureDetector(onTap: () {_onChangedTab(2);}, child: NavigationDestination(selected: _selectedTab == 2, svgPathSelected: "assets/icons/navbar-home-selected.svg" , svgPath: "assets/icons/navbar-home.svg", label: "HOME",)),
-                  GestureDetector(onTap: () {_onChangedTab(3);}, child: NavigationDestination(selected: _selectedTab == 3, svgPathSelected: "assets/icons/navbar-home-selected.svg" , svgPath: "assets/icons/navbar-home.svg", label: "HOME",)),
-                  GestureDetector(onTap: () {_onChangedTab(4);}, child: NavigationDestination(selected: _selectedTab == 4, svgPathSelected: "assets/icons/navbar-home-selected.svg" , svgPath: "assets/icons/navbar-home.svg", label: "HOME",)),
+                  GestureDetector(onTap: () {_onChangedTab(2);}, child: NavigationDestination(selected: _selectedTab == 2, svgPathSelected: "assets/icons/navbar-messages-selected.svg" , svgPath: "assets/icons/navbar-messages.svg", label: "MESSAGES",)),
+                  GestureDetector(onTap: () {_onChangedTab(3);}, child: NavigationDestination(selected: _selectedTab == 3, svgPathSelected: "assets/icons/navbar-trips.svg" , svgPath: "assets/icons/navbar-trips.svg", label: "TRIPS",)),
+                  GestureDetector(onTap: () {_onChangedTab(4);}, child: NavigationDestination(selected: _selectedTab == 4, svgPathSelected: "assets/icons/navbar-profile-selected.svg" , svgPath: "assets/icons/navbar-profile.svg", label: "ACCOUNT",)),
                   ],
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class CarPreviewContainer extends StatelessWidget {
+  const CarPreviewContainer({
+    super.key,  });
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    Image carImage = Image.asset('assets/images/car-volkswagen.jpg', height: 120, width: 160, fit: BoxFit.cover,);
+    String carName = "Volkswagen Beetle";
+    return IntrinsicWidth(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(border: Border.all(color: Colors.grey, width: 0.5), borderRadius: BorderRadius.circular(22)),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(22),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              carImage,
+              CarInfoContainer(carName: carName, rating: 4.6, nbReviews: 122,)
+            ],),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CarInfoContainer extends StatelessWidget {
+  const CarInfoContainer({
+    super.key,
+    required this.carName,
+    required this.rating,
+    required this.nbReviews
+  });
+
+  final String carName;
+  final double rating;
+  final int nbReviews;
+  
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    TextStyle lightRoboto = GoogleFonts.roboto(fontSize: 12, fontWeight: FontWeight.w300, color: Color.fromARGB(255, 84, 84, 84));
+    return Padding(
+      padding: const EdgeInsets.only(top:4, bottom:4, left: 8, right: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(carName, style: GoogleFonts.roboto(fontSize: 12, fontWeight: FontWeight.w600, color: Colors.black),),
+          Row(
+            children: [
+              Text(rating.toString(), style: lightRoboto),
+              Padding(
+                padding: const EdgeInsets.only(left: 2, right: 2),
+                child: Icon(Icons.star, color: theme.primaryColor, size: 12),
+              ),
+              Text("(${nbReviews.toString()} reviews)", style: lightRoboto,)
+            ],
+          )
+        ],
       ),
     );
   }
@@ -134,8 +211,8 @@ class NavigationDestination extends StatelessWidget {
   Widget build(BuildContext context) {
     final Color greySelected = Color.fromARGB(255, 228, 228, 228);
     return Container(child: Padding(
-      padding: const EdgeInsets.only(top: 6.0, bottom: 6.0, left: 10, right: 10),
-      child: Column(children: <Widget>[SvgPicture.asset(selected ? svgPathSelected : svgPath, height: 35, width: 35,), Text(label, style: TextStyle(fontSize: 8, fontWeight: FontWeight.w600),),]),
+      padding: const EdgeInsets.only(top: 6.0, bottom: 6.0, left: 8, right: 8),
+      child: Column(children: <Widget>[SvgPicture.asset(selected ? svgPathSelected : svgPath, height: 35, width: 35,), Text(label, style: GoogleFonts.roboto(fontSize: 8, fontWeight: FontWeight.bold)),]),
     ), decoration: BoxDecoration(borderRadius: BorderRadius.circular(20), color: selected? greySelected : null),);
   }
 }
